00b415aa2f06e854e0d15781205a75e029aad590
Aymen Chetoui <ach@office.123hjemmeside.local>
2016-11-04 16:34:13 +0100

    18%

--- events/app/helpers/events_helper.rb
+++ events/app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def format_price(event)
+    if event.free?
+      content_tag(:strong, "Free!") # or "<strong>Free</strong>".html_safe # .html_safe tells rails to not escape strong tags in html
+    else
+      number_to_currency(event.price)
+    end
+  end
 end
--- events/app/models/event.rb
+++ events/app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+  def free? # business logic go also in Models
+    price.blank? || price.zero? # || means or, blank? and zero? are built in methods return boolean 
+  end
 end
